863de396bb6bde33fd5e71417eb5d93d810769bc
isimsek7 <isimsekmusic@gmail.com>
2024-07-18 03:33:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace IfElsePractice;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Lutfen bir sayi giriniz:");
+        int sayi= Convert.ToInt32(Console.ReadLine());
+        if(sayi>10)
+        {
+            Console.WriteLine("Sayi ondan buyuktur.");
+        }
+        else
+        {
+            Console.WriteLine("Sayi ondan kucuktur.");
+        }
+        if(sayi%2==0)
+        {
+            Console.WriteLine("Sayi cift sayidir.");
+        }
+        else
+        {
+            Console.WriteLine("Sayi tek sayidir.");
+        }
+
+    }
+}
+    
+
+    
